abb834fb0b37afd2b21b19f65f769af7195af3bd
alrasyidlettering <alrasyidlettering@gmail.com>
2025-10-20 13:36:19 +0700

    1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1319044989"
+    serviceWorkerVersion: "916659516"
   }
 });
